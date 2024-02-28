0992d5fd0660fe130d2222011f6686cfee58c2e6
Mohd Siraj <sirajmohd889@gmail.com>
2024-02-28 16:00:29 +0530

    comment

--- frontend/src/pages/shop/Shop.jsx
+++ frontend/src/pages/shop/Shop.jsx
@@ -15,7 +15,9 @@ const Shop = () => {
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await axios.get(`http://localhost:3001/api/dishes`);
+        const response = await axios.get(
+          `https://foodapp-xyfd.vercel.app/api/dishes`
+        );
         console.log(response.data);
         setProduct(response.data);
         setFoodOnly(response.data.filter((item) => item.category === "food"));
